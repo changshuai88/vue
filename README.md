0af5fb37ea8cc12ea7860b96253ba955959ce43e
2233282817@qq.com <2233282817@qq.com>
2021-09-19 18:27:22 +0800

    初步了解vue

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue
 study vue
+
+# vue3 地址：
+`<script src="https://unpkg.com/vue@next"></script>`
---
+++ vue3demo02/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello 沃泰机械" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
