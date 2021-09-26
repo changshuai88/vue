96f3bd84c0947c5bc93bd761ff80f59c5a64dcd0
2233282817 <2233282817@qq.com>
2021-09-26 14:28:08 +0800

    v-html

---
+++ vue3demo04/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h1>{{msg}}</h1>
+<h1 @click="changeMsg" v-once>{{msg}}</h1>
+<div>{{content}}</div>
+<div v-html="content"></div>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      msg:"hello world",
+      content:"<h1>标题</h1><h2>小标题</h2>"
+    }
+  },
+  methods:{
+    changeMsg(){
+      this.msg="蛋炒饭"
+    }
+  }
+}
+</script>
