0e2c825e52df51e178283c56a891d2ae036d18b4
2233282817@qq.com <2233282817@qq.com>
2021-12-03 21:07:54 +0800

    逗号的错误是因为分号

--- demo/src/components/Banner.vue
+++ demo/src/components/Banner.vue
@@ -1,39 +1,52 @@
 <template>
-<button class="btn">111</button>
-<div class="b_contain">
-  <ul>
-    <li><img src="http://img4.yun300.cn/repository/image/-RnBPFpbRJqcFMui073kPQ.jpg?tenantId=132892&viewType=1" alt=""></li>
-    <li><img src="http://img4.yun300.cn/repository/image/-RnBPFpbRJqcFMui073kPQ.jpg?tenantId=132892&viewType=1" alt=""></li>
-    <li><img src="http://img4.yun300.cn/repository/image/-RnBPFpbRJqcFMui073kPQ.jpg?tenantId=132892&viewType=1" alt=""></li>
-  </ul>
-</div>
-  <img :src="imgsrc" alt="">
+  <button class="btn">111</button>
+  <div class="b_contain">
+    <!-- <ul>
+      <li>
+        <img
+          src="http://img4.yun300.cn/repository/image/-RnBPFpbRJqcFMui073kPQ.jpg?tenantId=132892&viewType=1"
+          alt=""
+        />
+      </li>
+      <li>
+        <img
+          src="http://img4.yun300.cn/repository/image/-RnBPFpbRJqcFMui073kPQ.jpg?tenantId=132892&viewType=1"
+          alt=""
+        />
+      </li>
+      <li>
+        <img
+          src="http://img4.yun300.cn/repository/image/-RnBPFpbRJqcFMui073kPQ.jpg?tenantId=132892&viewType=1"
+          alt=""
+        />
+      </li>
+    </ul> -->
+  </div>
+  <img :src="imgsrc" alt="" />
 </template>
 
 <script>
 export default {
-  name: 'banner',
-  data (){
+  name: "banner",
+  data() {
     return {
-      imgsrc: require('assets/images/1.jpeg'),
-    },
+      imgsrc: require("assets/images/1.jpeg"),
+    };
   },
-  computed:{
-
-  }
+  computed: {},
 };
 </script>
 
 <style scoped lang="scss">
-.b_contain{
+.b_contain {
   margin: 0 auto;
   width: 100%;
-  ul{
+  ul {
     list-style: none;
-    li{
-    display: inline-block;
-      img{
-       width: 100%;
+    li {
+      display: inline-block;
+      img {
+        width: 100%;
       }
     }
   }
