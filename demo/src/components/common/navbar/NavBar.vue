22ed5da615aa68cf15e5be568f7a3495d3e4406d
2233282817@qq.com <2233282817@qq.com>
2021-12-06 21:27:11 +0800

    navbar导航插槽

---
+++ demo/src/components/common/navbar/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot>GoodsShop</slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
--- demo/src/views/home/Home.vue
+++ demo/src/views/home/Home.vue
@@ -1,32 +1,35 @@
 <template>
   <div class="home">
-    <Banner />
+    <nav-bar>
+      <template v-slot:left>&lt;</template>
+      <template v-slot:default>图书兄弟</template>
+    </nav-bar>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
-import { onMounted } from "vue";
-import { getHomeAllData, add } from "network/home";
-import Banner from "components/Banner.vue";
+// import { onMounted } from "vue";
+// import { getHomeAllData, add } from "network/home";
+import NavBar from "components/common/navbar/NavBar.vue";
 
 export default {
   name: "Home",
   setup() {
-    onMounted(() => {
-      add(1, 2);
-      getHomeAllData()
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log("err");
-        });
-    });
+    // onMounted(() => {
+    //   add(1, 2);
+    //   getHomeAllData()
+    //     .then((res) => {
+    //       console.log(res);
+    //     })
+    //     .catch((err) => {
+    //       console.log("err");
+    //     });
+    // });
   },
   components: {
-    Banner,
+    NavBar,
   },
 };
 </script>
